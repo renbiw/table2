774053e90215405af061293a60c8c4855872a619
renbiw <48404799+renbiw@users.noreply.github.com>
2019-04-15 12:36:27 +0300

    Add files via upload

---
+++ tablEeee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n    |   1\t2\t3\t4\t5\t6\t7\t8\t9");
+            Console.Write("__________________________________________________________________________");
+            Console.WriteLine();
+            for (int i = 1; i < 10; i++)
+            {
+                Console.Write(i + "   |  ");
+                for (int j = 1; j < 10; j++)
+                {
+                    Console.Write("\t" + i * j);
+                }
+                Console.WriteLine("");
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+        }
+       
+    }
+}
